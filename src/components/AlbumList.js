5ac420ad8051eca8ca6bb890ac734ee6e845f987
Mary Jenel <myersjenel@gmail.com>
2018-06-08 16:46:58 -0700

    Use Relay to store data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { Text, ScrollView } from "react-native";
 import axios from "axios";
+import Album from "../container/AlbumContainer";
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -12,10 +13,12 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map(album => <Text>{album.title}</Text>);
+    return this.state.albums.map(album => (
+      <Album key={album.title} album={album} />
+    ));
   }
   render() {
-    return <View>{this.renderAlbums()}</View>;
+    return <ScrollView>{this.renderAlbums()}</ScrollView>;
   }
 }
 
